57b39bfa04d550d7df41f82f9d5eb143e6d37e4a
sid-bhatt <70953578+sid-bhatt@users.noreply.github.com>
2021-05-20 00:59:27 +0530

    Trying Sandbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+
+// import reportWebVitals from './reportWebVitals';
